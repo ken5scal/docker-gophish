bcd231f9a2362aa9cb911dda60926bb56028887b
ken5scal <kengoscal@gmail.com>
2017-04-19 12:11:20 +0900

    Removed unnecessary line

--- gcp_instances.tf
+++ gcp_instances.tf
@@ -1,10 +1,3 @@
-// Configure the Google Cloud provider
-provider "google" {
-    credentials = "${file("account.json")}"
-    project     = "apt-mail-training"
-    region      = "asia-northeast"
-}
-
 resource "google_compute_instance" "aptafdasfas" {
     name = "default"
     machine_type = "n1-standard-1" //This is Default
