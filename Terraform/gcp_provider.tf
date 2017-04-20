cc3390b02a6da62480fcd0d03cd09fca55be2114
ken5scal <kengoscal@gmail.com>
2017-04-20 23:25:02 +0900

    Add billing and organization information

--- Terraform/gcp_provider.tf
+++ Terraform/gcp_provider.tf
@@ -1,7 +1,9 @@
 // Configure the Google Cloud provider
 provider "google" {
-    credentials = "${file("account.json")}"
-    project     = "apt-mail-training"
-    region      = "asia-northeast"
+    credentials     = "${file("account.json")}"
+    project         = "apt-mail-training"
+    region          = "asia-northeast"
+    billing_acount  = "${var.billing_id}"
+    org_id          = "${var.org_id}"
 }
 
