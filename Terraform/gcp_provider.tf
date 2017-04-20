811d01bbe89afd940c5abf08137217707d735605
ken5scal <kengoscal@gmail.com>
2017-04-20 23:59:40 +0900

    Remove unnecesary resources

--- Terraform/gcp_provider.tf
+++ Terraform/gcp_provider.tf
@@ -3,6 +3,4 @@ provider "google" {
     credentials     = "${file("account.json")}"
     project         = "${var.project_id}"
     region          = "${var.region}"
-    billing_acount  = "${var.billing_id}"
-    org_id          = "${var.org_id}"
 }
