ec7285ec0d240cd7d1375747df21298632e1d3e1
ken5scal <kengoscal@gmail.com>
2017-04-20 23:46:48 +0900

    Update provider.tf so it can read from variables

--- Terraform/gcp_provider.tf
+++ Terraform/gcp_provider.tf
@@ -1,9 +1,8 @@
 // Configure the Google Cloud provider
 provider "google" {
     credentials     = "${file("account.json")}"
-    project         = "apt-mail-training"
-    region          = "asia-northeast"
+    project         = "${var.project_id}"
+    region          = "${var.region}"
     billing_acount  = "${var.billing_id}"
     org_id          = "${var.org_id}"
 }
-
