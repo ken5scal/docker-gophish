2fca1e3df121e2ad72126f53c43255eaff4dd8cf
ken5scal <kengoscal@gmail.com>
2017-04-20 22:38:32 +0900

    Add tag, description and disk information for instance

--- Terraform/gcp_instances.tf
+++ Terraform/gcp_instances.tf
@@ -1,10 +1,11 @@
-resource "google_compute_instance" "aptafdasfas" {
-    name = "default"
-    machine_type = "n1-standard-1" //This is Default
-    zone = "asia-northeast1-a"
-    
+resource "google_compute_instance" "default" {
+    name            = "default"
+    machine_type    = "n1-standard-1" //This is Default
+    zone            = "asia-northeast1-a"
+    description     = "apt-mail-training"
+    tags            = ["internal-use-only", "gophish", "go"]
     disk {
-
+        image = "debian-cloud/debian-8"
     }
 
     network_interface {
