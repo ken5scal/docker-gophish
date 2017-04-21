6ed1fa062272f179814317cf567b424ad5fb0ea2
ken5scal <kengoscal@gmail.com>
2017-04-21 16:00:46 +0900

    Add node configuration in file

--- Terraform/gcp_clusters.tf
+++ Terraform/gcp_clusters.tf
@@ -1,10 +1,19 @@
 resource "google_container_cluster" "primary" {
     name    = "gophish-cluster"
     zone    = "${var.zone}"
-    initial_node_count = 2
+    initial_node_count = 3
 
     additional_zones = [
         "asia-northeast1-b",
         "asia-northeast1-c",
     ]
+
+    node_config {
+        oauth_scopes = [
+        "https://www.googleapis.com/auth/compute",
+        "https://www.googleapis.com/auth/devstorage.read_only",
+        "https://www.googleapis.com/auth/logging.write",
+        "https://www.googleapis.com/auth/monitoring",
+        ]
+    }
 }
